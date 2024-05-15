63a3291539020a596270a63451dc2c8e70f568ca
Shani Soni <shanisoni7084@gmail.com>
2024-05-15 22:40:15 +0530

    committed

--- src/App.js
+++ src/App.js
@@ -1,33 +1,60 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
 
 function App() {
-  const[variable , setVariable] = useState( 0)
+  const [variable, setVariable] = useState(0);
+  const [loading, setLoading] = useState(false);
 
+  const handleplus = () => {
+    setLoading(true);
+    setTimeout(() => {
+      setVariable(variable + 1);
+      setLoading(false);
+    }, 2000);
+  };
 
   return (
-    <div>
+    <div className="w-[100vw] h-[100vh] flex justify-center items-center bg-[#344151] flex-col gap-10">
+      <div className="font-medium text-2xl bg-white flex justify-center gap-12 py-3 rounded-sm text-[25px] text-[#344151] ">
+        <div className="text-[#0398d4] font-medium text-2xl ">
+          Increment & Decrement
+        </div>
 
-      <div>
-        Increment & Decrement
+        <div>
+          <button
+            className=" border-r-2 text-center w-20 border-[#bfbfbf] text-5xl "
+            onClick={() => {
+              setVariable(variable - 1);
+            }}
+          >
+            -
+          </button>
+        </div>
+
+        <div className="text-[#0398d4]  text-5xl  gap-12 font-bold ">
+          {variable}
+        </div>
+
+        <div>
+          <button
+            className=" border-r-2 text-center w-20 border-[#bfbfbf] text-5xl "
+            onClick={handleplus}
+          >
+            +
+          </button>
+        </div>
+
+        <button>
+          <button
+            className="text-white font-medium text-2xl bg-[#0398d4] px-5 py-2 rounded-sm text-lg "
+            onClick={() => setVariable(0)}
+          >
+            Restart
+          </button>
+        </button>
       </div>
-
-      <div>
-        -
-      </div>
-
-      <div>
-
-      </div>
-
-      <div>
-        +
-      </div>
-
-      <button>
-        Restart
-      </button>
+      {loading ? <div>Loading...</div> : ""}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
